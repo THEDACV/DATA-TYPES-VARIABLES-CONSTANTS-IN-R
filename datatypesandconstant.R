8bdf49c84e7091473a49e53e2a3600321fde1a30
chaithanya vishwamitra D A <163235188+THEDACV@users.noreply.github.com>
2024-08-23 15:46:56 +0530

    Add files via upload

---
+++ datatypesandconstant.R
@@ -0,0 +1,17 @@
+#constants
+PI<-3.14
+GRAVITY<-9.81
+
+#Data Types
+name<-"dacv"
+age<-20
+height<-5.7
+is_student<-T
+
+#display all the values 
+cat("name:",name,"\n")
+cat("age:",age,"\n")
+cat("height:",height,"feet\n")
+cat("is_student:",is_student,"\n")
+cat("PI:",PI,"\n")
+cat("GRAVITY:",GRAVITY,"m/s^2\n")
